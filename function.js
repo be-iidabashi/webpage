0764da5aa2013f1561ed54d84688c4bb6c925ef3
katsukikuroda <katsukikuroda@gmail.com>
2025-01-05 15:14:06 +0900

    js change

--- function.js
+++ function.js
@@ -6,8 +6,10 @@ let count = 0;
 
 function timer(){
     count += 1;
-    if (count % 2 === 1) {
+    if (count % 3 === 1) {
         subtitle.style.color = 'red';
+    } else if (count % 3 === 2) {
+        subtitle.style.color = 'green';
     } else {
         subtitle.style.color = 'black';
     }
